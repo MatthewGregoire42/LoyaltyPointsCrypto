466492d441f56d98f68b314685211fe000e26c40
Matthew Gregoire <mattyg@live.unc.edu>
2023-05-11 13:54:58 -0400

    Fixed enc/dec errors, started interfacing with Python

--- python/crypto/crypto.py
+++ python/crypto/crypto.py
@@ -1,8 +1,38 @@
 import _crypto
 import cryptography
 
-print("Hello! test")
+"""
+TODO:
+1. write wrappers for the Rust crypto functions
+2. write a simple benchmark file (just encrypt/decrypt one message? Maybe add a proof as well)
+3. once that works, put it up on the Google cloud machine
+"""
 
-_crypto.guess_the_number()
+class Server:
+    num_users = 0
+    field = 0
 
-print(_crypto.mult(7, 14))
+    def __init__(self):
+        pass
+
+class Client:
+    field = 0
+    def __init__(self):
+        pass
+
+    def process_tx_hello(self):
+        pass
+    def process_tx_response(self):
+        pass
+
+    def settle_balance(self):
+        pass
+
+sk, pk = _crypto.elgamal_keygen()
+print(pk, sk)
+print("---")
+ct = _crypto.elgamal_enc(pk, -17) # ct has y as is third value, scrape off before passing to dec
+print(ct)
+print("---")
+pt = _crypto.elgamal_dec(sk, ct[:2])
+print(pt)
--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,19 @@ use rand_core::OsRng;
 use curve25519_dalek::constants;
 use curve25519_dalek::ristretto::CompressedRistretto;
 use curve25519_dalek::ristretto::RistrettoPoint;
+use curve25519_dalek::ristretto::RistrettoBasepointTable;
 use curve25519_dalek::scalar::Scalar;
 use sha2::Sha512;
 use curve25519_dalek::digest::Update;
+use hex::encode;
+
+const G: &RistrettoBasepointTable = &constants::RISTRETTO_BASEPOINT_TABLE;
 
 // Returns a compressed version of a tuple (Scalar, RistrettoPoint)
 #[pyfunction]
 fn elgamal_keygen() -> ([u8; 32], [u8; 32]) {
     let x: Scalar = Scalar::random(&mut OsRng);
-    let h: RistrettoPoint = &x * &constants::RISTRETTO_BASEPOINT_TABLE;
+    let h: RistrettoPoint = &x * G;
     (x.to_bytes(), h.compress().to_bytes())
 }
 
@@ -20,14 +24,20 @@ fn elgamal_keygen() -> ([u8; 32], [u8; 32]) {
 //      m:  a message to encrypt (number of loyalty points)
 // Returns a compressed version of a tuple (RistrettoPoint, RistrettoPoint, Scalar)
 #[pyfunction]
-fn elgamal_enc(pk: [u8; 32], m: u32) -> ([u8; 32], [u8; 32], [u8; 32]) {
+fn elgamal_enc(pk: [u8; 32], m: i32) -> ([u8; 32], [u8; 32], [u8; 32]) {
     let pk = CompressedRistretto::from_slice(&pk).decompress().unwrap();
     let y: Scalar = Scalar::random(&mut OsRng);
-    let g = &constants::RISTRETTO_BASEPOINT_TABLE;
-    let m: Scalar = Scalar::from(m);
 
-    let c1 = &y*g;
-    let c2 = &m*g + y*pk;
+    // We need to convert m to a scalar
+    let m_pos: u32 = m.abs() as u32;
+    let m_scalar = if m == m_pos as i32 { Scalar::from(m_pos) }
+                   else { Scalar::zero() - Scalar::from(m_pos) };
+    // println!("m == m_pos? {}", m == m_pos as i32);
+
+    let c1 = &y*G;
+    let c2 = &m_scalar*G + y*pk;
+
+    // println!("Actual m*g: {}", encode((&m_scalar*G).compress().to_bytes()));
 
     // y is a secret; it needs to be kept by the client to generate a proof
     // and then is discarded afterwards.
@@ -39,22 +49,31 @@ fn elgamal_enc(pk: [u8; 32], m: u32) -> ([u8; 32], [u8; 32], [u8; 32]) {
 //      ct:  a compressed (RistrettoPoint, RistrettoPoint) ciphertext
 // Returns the decrypted number of loyalty points
 #[pyfunction]
-fn elgamal_dec(sk: [u8; 32], ct: ([u8; 32], [u8; 32])) -> u32 {
+fn elgamal_dec(sk: [u8; 32], ct: ([u8; 32], [u8; 32])) -> i32 {
     let sk = Scalar::from_bytes_mod_order(sk);
     let ct0 = CompressedRistretto::from_slice(&ct.0).decompress().unwrap();
     let ct1 = CompressedRistretto::from_slice(&ct.1).decompress().unwrap();
-    let g = &constants::RISTRETTO_BASEPOINT_TABLE;
-    let mg = ct1 + sk.invert() * ct0;
+    let mg = ct1 + (Scalar::zero() - sk) * ct0;
+    // println!("mg: {}", encode(mg.compress().to_bytes()));
 
-    let mut m: u32 = 0;
+    // println!("m*g: {}", encode((&Scalar::from(17u32) * G).compress().to_bytes()));
+
+    // println!("zero: {}", encode(Scalar::zero().to_bytes()));
+    // println!("zero (hopefully): {}", encode((Scalar::from(17u32) +
+    //     (Scalar::zero() - Scalar::from(17u32))).to_bytes()));
 
     // The result of ElGamal decryption (mg) is the value m*g. Assuming m is a small scalar,
     // we can extract it by guessing and checking different values until we find an m
     // such that mg = m*g.
+    let mut m: u32 = 0;
     loop {
-        if &Scalar::from(m) * g == mg {
-            break m;
-        } else {
+        if &Scalar::from(m) * G == mg {
+            break m as i32;
+        } else if &(Scalar::zero() - Scalar::from(m)) * G == mg {
+            break -1 * (m as i32);
+        } else if m > 10000 {
+            panic!("Looping too long");
+        } {
             m += 1;
         }
     }
@@ -122,7 +141,6 @@ struct CompressedCtEqProof {
 #[pyfunction]
 fn zk_ct_eq_prove(shopper_tx: CompressedTxCiphertextData, barcode_tx: CompressedTxCiphertextData)
                  -> CompressedCtEqProof {
-    let g = &constants::RISTRETTO_BASEPOINT_TABLE;
     
     let shopper_tx: TxCiphertextData = shopper_tx.decompress();
     let barcode_tx: TxCiphertextData = barcode_tx.decompress();
@@ -144,11 +162,11 @@ fn zk_ct_eq_prove(shopper_tx: CompressedTxCiphertextData, barcode_tx: Compressed
     let ys_t = Scalar::random(&mut OsRng);
     let yb_t = Scalar::random(&mut OsRng);
 
-    let cs0_t = &ys_t*g;
-    let cs1_t = &mp_t*g + ys_t*hs;
-    let cb0_t = &yb_t*g;
-    let cb1_t = &m_t*g + yb_t*hb;
-    let i_t = &m_t*g + &mp_t*g;
+    let cs0_t = &ys_t*G;
+    let cs1_t = &mp_t*G + ys_t*hs;
+    let cb0_t = &yb_t*G;
+    let cb1_t = &m_t*G + yb_t*hb;
+    let i_t = &m_t*G + &mp_t*G;
 
     // Challenge
     let mut hasher = Sha512::default();
@@ -192,8 +210,6 @@ fn zk_ct_eq_verify(pi: CompressedCtEqProof) -> bool {
     }
     let c = Scalar::from_hash(hasher);
 
-    let g = &constants::RISTRETTO_BASEPOINT_TABLE;
-
     let cs0 = CompressedRistretto::from_slice(&pi.shopper_ct.0).decompress().unwrap();
     let cs1 = CompressedRistretto::from_slice(&pi.shopper_ct.1).decompress().unwrap();
     let cb0 = CompressedRistretto::from_slice(&pi.barcode_ct.0).decompress().unwrap();
@@ -210,11 +226,11 @@ fn zk_ct_eq_verify(pi: CompressedCtEqProof) -> bool {
     let hs = CompressedRistretto::from_slice(&pi.hs).decompress().unwrap();
     let hb = CompressedRistretto::from_slice(&pi.hb).decompress().unwrap();
 
-    let check1 = g * &ys_z == cs0_t + cs0 * &c;
-    let check2 = g * &mp_z + hs*&ys_z == cs1_t + cs1 * &c;
-    let check3 = g * &yb_z == cb0_t + cb0 * &c;
-    let check4 = g * &m_z + hb * &yb_z == cb1_t + cb1 * &c;
-    let check5 = g * &m_z + g * &mp_z == i_t;
+    let check1 = G * &ys_z == cs0_t + cs0 * &c;
+    let check2 = G * &mp_z + hs*&ys_z == cs1_t + cs1 * &c;
+    let check3 = G * &yb_z == cb0_t + cb0 * &c;
+    let check4 = G * &m_z + hb * &yb_z == cb1_t + cb1 * &c;
+    let check5 = G * &m_z + G * &mp_z == i_t;
 
     check1 && check2 && check3 && check4 && check5
 }
